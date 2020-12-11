e1a3e0e05505e83de5f2dfc6d34b12efcb4480d0
Mohammad Sadegh Shad <mohammadsadegh.shad@geeks.ltd.uk>
2020-12-11 15:29:53 +0330

    Update

--- push-to-nuget.bat
+++ push-to-nuget.bat
@@ -1,1 +1,3 @@
+#dotnet clean
+#dotnet build --configuration Release
 dotnet nuget push ".\bin\Release\*.nupkg" --source https://api.nuget.org/v3/index.json --api-key oy2h4qwjsox5nhvhxtnaesleoq7hbn5ujpk3ci4fj266za
